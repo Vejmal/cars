98fdcfb9f8fe39a1b0130199505840644d5470ef
Tomasz Leśniczuk <tomasz.lesniczuk@outlook.com>
2022-10-22 23:37:42 +0200

    min and max info about cars

--- frontend/src/components/Cars/Car/Car.js
+++ frontend/src/components/Cars/Car/Car.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Car(props) {
 
+  const [showDesc, setShowDesc] = useState(false);
+
+  const toggleDesc = () => {
+    setShowDesc(!showDesc)
+  }
+
   return (
     <div className='car mark'>
-          <p>{props.mark}</p>
-          <div className='car model'></div>
-          <p>{props.model}</p>
-          <div className='car engine-capacity'></div>
-          <p>{props.engineCapacity}</p>
-          <div className='car horse-power'></div>
-          <p>{props.horsePower}</p>
+          <p onClick={toggleDesc}>{props.mark}</p>
+          {showDesc && (
+            <>
+            <div className='car model'></div>
+            <p>{props.model}</p>
+            <div className='car engine-capacity'></div>
+            <p>{props.engineCapacity}</p>
+            <div className='car horse-power'></div>
+            <p>{props.horsePower}</p>
+            </>
+          )}
           <button>edytuj</button>
           <button className='delete' onClick={() => props.onDelete(props.id)}>usuń</button>
         </div>
